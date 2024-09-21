7900b69cb305eef2c37b99857504b903d6056e46
Matt Fryer <MattFryer@users.noreply.github.com>
2024-09-21 22:52:39 +0100

    Added comments to config to explain settings

--- Firmware-ESP/include/config.h
+++ Firmware-ESP/include/config.h
@@ -1,6 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H 1
 
-#define DEVICE_ID 0x317E33
+// Device ID for home assistant. Shouldn't need to change unless there is a 
+// clash with another home assistant device or you need more than one ws2mqtt instance.
+#define DEVICE_ID 0x317E33 
+
+// How often should new devices be queried
 #define QUERY_NEW_DEVICES_INTERVAL 60 // seconds
 #endif
